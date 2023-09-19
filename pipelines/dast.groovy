4ff08907dae8984b9958fb335118f69428d549e1
Raffaele Stefanile <raffaele.stefanile@capgemini.com>
2023-09-20 01:10:58 +0200

    test

--- pipelines/dast.groovy
+++ pipelines/dast.groovy
@@ -1,7 +1,8 @@
 pipeline {
-    agent { label 'windows' }
+    agent any
     stages {
         stage('APPScan: DAST demo') {
+            agent { node { label 'windows' } }
             steps {
                 bat "AppScanCMD e /su http://demo.testfire.net"
             }
